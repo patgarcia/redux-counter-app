9a9596852027241cfa2b84fbeec4f4bf89e7e261
patgarcia <commonzenmedia@gmail.com>
2021-11-02 15:03:18 -0400

    Memoize useActions hook

--- counter/src/actions/useActions.js
+++ counter/src/actions/useActions.js
@@ -1,7 +1,11 @@
+import { useMemo } from 'react';
 import { useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 export const useActions = actions => {
   const dispatch = useDispatch();
-  return bindActionCreators(actions, dispatch);
+  return useMemo(
+    () => bindActionCreators(actions, dispatch),
+    [actions, dispatch]
+  );
 };
